aa6e778f46640e3b62a54b052fb5b7a7dd1b8db1
Kevin Hernández Rostrán <98577482+profe-kevs@users.noreply.github.com>
2023-02-09 21:48:48 +0000

    feat: Add first_condition

--- test_main.py
+++ test_main.py
@@ -1,6 +1,12 @@
-def func(x):
-    return x + 1
+from main import first_condition, translate
 
 
-def test_answer():
-    assert func(3) == 5
+def test_first_condition():
+    assert first_condition("ad") == True
+    assert first_condition("xr") == True
+    assert first_condition("yt") == True
+
+def test_translate_with_first_condition():
+    assert translate("adjective") == "adjectiveay"
+    assert translate("xray") == "xrayay"
+    assert translate("yttria") == "yttriaay"
